3f64b5ea08593cd1c45d44f96cf85b78727e0a5c
Lê  Hoàng Vũ <68913@thegioididong.com>
2023-05-11 11:10:08 +0700

    done design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,10 +14,19 @@ const Hero = () => {
                 <button
                     type='button'
                     onClick={() => window.open('https://github.com/thamtumeoden13')}
+                    className='black_btn'
                 >
                     Github
                 </button>
             </nav>
+            <h1 className='head_text'>
+                Summarize Articles with <br className='max-md:hidden' />
+                <span className='orange_gradient'>OpenAI GPT-4</span>
+            </h1>
+            <h2 className='desc'>
+                Simplify your reading with Summize, an open-source article summarizer
+                that transforms length articles into clear and concise summaries.
+            </h2>
         </header>
     )
 }
