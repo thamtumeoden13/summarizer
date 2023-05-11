afa769ba581e1cd552c37ff171e29b86f6eb1506
Lê  Hoàng Vũ <68913@thegioididong.com>
2023-05-11 13:17:27 +0700

    donte RTK

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
+import { useLazyGetSummaryQuery } from '../services/article'
 import { copy, linkIcon, loader, tick } from '../assets'
 
 const Demo = () => {
@@ -8,10 +9,44 @@ const Demo = () => {
         url: '',
         summary: '',
     })
+    const [allArticles, setAllArticles] = useState([])
+    const [copied, setCopied] = useState(false)
+
+    const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery()
 
     const handleSubmit = async (e) => {
-        alert('Submmited')
+        e.preventDefault()
+
+        const { data } = await getSummary({ articleUrl: article.url })
+
+        if (data?.summary) {
+            const newArticle = { ...article, summary: data.summary }
+            const updateAllArticles = [newArticle, ...allArticles]
+
+            setArticle(newArticle)
+            setAllArticles(updateAllArticles)
+
+            localStorage.setItem('articles', JSON.stringify(updateAllArticles))
+
+            console.log(newArticle)
+        }
     }
+
+    const handleCopy = (copyUrl) => {
+        setCopied(copyUrl)
+        navigator.clipboard.writeText(copyUrl)
+
+        setTimeout(() => {
+            setCopied('')
+        }, 3000);
+    }
+
+    useEffect(() => {
+        const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'))
+        if (articlesFromLocalStorage) {
+            setAllArticles(articlesFromLocalStorage)
+        }
+    }, [])
 
     return (
         <section className='mt-16 w-full max-w-xl'>
@@ -48,10 +83,61 @@ const Demo = () => {
                 </form>
 
                 {/* Brower URL History */}
-
+                <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+                    {allArticles.map((article, index) => (
+                        <div
+                            key={`article-${index}`}
+                            onClick={() => setArticle(article)}
+                            className='link_card'
+                        >
+                            <div className='copy_btn' onClick={() => handleCopy(article.url)}>
+                                <img
+                                    src={copied === article.url ? tick : copy}
+                                    alt='copy_icon'
+                                    className='w-[40%] h-[40%] object-contain'
+                                />
+                            </div>
+                            <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+                                {article.url}
+                            </p>
+                        </div>
+                    ))}
+                </div>
             </div>
 
             {/* Display Result */}
+            <div className='my-10 max-w-full flex justify-center items-center'>
+                {isFetching ? (
+                    <img
+                        src={loader}
+                        alt='loader'
+                        className='w-20 h-20 object-contain'
+                    />
+                ) : error ? (
+                    <p className='font-inter font-bold text-black text-center'>
+                        Well, that wasn't supposed to happen...
+                        <br />
+                        <span className=' font-satoshi font-normal text-gray-700'>
+                            {error?.data?.error}
+                        </span>
+                    </p>
+                ) : (article.summary && (
+                    <div className='flex flex-col gap-3'>
+                        <h2 className=' font-satoshi font-bold text-gray-600 text-xl'>
+                            Article &nbsp;
+                            <span className='blue_gradient'>
+                                Summary
+                            </span>
+                        </h2>
+                        <div className='summary_box'>
+                            <p className='font-inter font-medium text-sm text-gray-700'>
+                                {article.summary}
+                            </p>
+                        </div>
+                    </div>
+                ))}
+
+            </div>
         </section>
     )
 }
